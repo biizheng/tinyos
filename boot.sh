cdcce002f801103e127350a289e856abd8e2efdb
biizheng <m15840149205@163.com>
2021-04-18 07:09:55 +0800

    add comments to the compiling shell script.

--- boot.sh
+++ boot.sh
@@ -5,9 +5,17 @@
 #nasm boot.asm -o boot.bin -l.lst 
 nasm $1.asm -o $1.bin -l $1.lst 
 
+# write your compiled progarm into a virtual iamge
+# if    input file: in this case ,the input file is "$1.bin"
+# of    output file: in this case ,the output file is "$1.img"
+# bs    block size: the size of block you are going to transfer
+# count the number of blocks that will be transferd
+# conv  "conv=notrunc" means it will not truncate the output file 
+#       even if the input file is smaller than the output file
 dd if=$1.bin of=$1.img bs=512 count=1 conv=notrunc
 
 # -q    skip the "Bochs Configuration Main menu"
-# -f    to define a configuration file 
-# -rc   
+# -f    to define a configuration file
+# -rc   you can put the instruct you want to execute 
+#       when your program in bochs start up
 bochs -q -f ./bochsrc.properties -rc ./run.cfg
